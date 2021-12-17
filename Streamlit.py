d4b3d6edf7816334e583f431972228f8833dfba6
Jothy Selvaraj <jothybasu@gmail.com>
2021-12-17 14:02:15 +1100

    Streamlit.py added

--- Streamlit.py
+++ Streamlit.py
@@ -1,11 +1,22 @@
 import streamlit as st
 
-st.title('QAT+ Scripts')
+st.markdown("<h1 style='text-align: center; color: white;'>QATrack+ Scripts</h1>",unsafe_allow_html=True)
 st.sidebar.title("Welcome to QAT+ Scripts")
-st.sidebar.subheader('')
+st.sidebar.subheader('------QATrack+ automation------')
+
+def test():
+    st.write('testing...')
 
 option = st.selectbox(
      'Select user',
-    ('Jothy Selvaraj', 'Jon Lee', 'Ravi Thura','Helen Gustafsson','Kim Legge','kasia Bobrowski','Brendan Wright','Nigel Freeman','Ben Cooper'))
+    ('Brendan Wright','Ben Cooper','Helen Gustafsson','Jothy Selvaraj', 'Jon Lee','Kim Legge','kasia Bobrowski', 'Nigel Freeman','Ravi Thura'))
 
 st.write('You selected:', option)
+
+st.error("Do you really, really, wanna do this?")
+if st.button("Yes I'm ready to rumble"):
+    test()
+
+
+
+
