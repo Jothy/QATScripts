779db9637c3ebfb55103c6813429c3014ff827fb
Jothy Selvaraj <Jothybasu@gmail.com>
2021-12-23 19:11:07 +1100

    ScriptStreamlit.py updated with tryLogin()

--- ScriptStreamlit.py
+++ ScriptStreamlit.py
@@ -4,23 +4,21 @@ from ScriptAPI import QAT_API as api
 
 url = "https://canberra-staging.multileaf.ca/api"
 timeout = 5
-token = "00754303d15f0cd9c49a8606a02a741eac5bf2d1c9"
+token = "754303d15f0cd9c49a8606a02a741eac5bf2d1c9++"
 auth = {"Authorization": "Token %s" % token}
 
 
 def tryLogin():
-    request = requests.get(url, headers=auth)
-    if(request.json()['detail']!='invalid token'):
-        st.success('Connected to '+url)
+    resp = requests.get(url, headers=auth)
+    if 200 <= resp.status_code <= 299:
+        st.write('Login Success!')
     else:
-        st.error("Login failed")
+        st.error('Login failed!')
+
 
 st.markdown("<h1 style='text-align: center; color: white;'>QATrack+ Scripts</h1>",unsafe_allow_html=True)
 st.sidebar.title("Welcome to QAT+ Scripts")
 st.sidebar.subheader('------QATrack+ automation------')
-
-def test():
-    st.write('testing...')
 
 option = st.selectbox(
      'Select user',
